b734bd719c4b080953e4c61b3c307fd4bc3c89cb
qlovy <qlovy@sysnet.ch>
2024-01-07 14:32:55 +0100

    Ajout interface avec fonctionnement ok

--- timerV2.py
+++ timerV2.py
@@ -2,22 +2,58 @@
 Timer V2 is a timer with a new window appear on your screen to tell you when it's finish
 """
 
-import time
-import tkinter as tk
+import time  # for manage the time
+from tkinter import * # for the user interface  
 
-while True:
-    try:
-        timeChosen = int(input("\nSelcet the time for the timer (in seconds pls)"))
-    except:
-        print("\nMake sur to write an numbre")
+def callback (input):
+    if input.isdigit():
+        return True
+    elif input is "":
+        return True
     else:
-        print("\nThe timer starts now")
-        time.sleep(timeChosen)
-        # the new window
-        window = tk.Tk()
-        title = tk.Label(text = "Your timer is finished")
-        title.pack()
-        window.mainloop()
+        return False
 
+def launch():
+    if timeChoose.get().isdigit():
+        timeElapse = int(timeChoose.get())
+        print(timeElapse)
+        #start_message = Label(root, text="Start timer").pack(side=BOTTOM)
+        # new window
+        countDown = Tk()
+        countDown.title("Timer count down")
+        countDown.geometry("300x300+400+50")
+        time.sleep(timeElapse)
+        end_message = Label(countDown, text="Your timer is finish").pack()
+        countDown.mainloop()
+
+# USER INTERFACE
+
+# root window
+root = Tk()
+root.title("Timer V2")
+# set minimum and the maximum size of the window
+root.minsize(200, 200) # width, height
+root.maxsize(500, 500)
+# set the window to theses dimensions
+root.geometry("300x300+50+50") # width x height + x + y
+
+# label
+title = Label(root, text="Welcome in your timer", font=("Ubuntu", 20), padx=10,pady=10).pack()
+sub_title = Label(root, text="Select the time to elapse (seconds)", pady=10).pack()
+
+# Entry
+timeChoose = Entry(root, foreground="black", font=("Ubuntu"))
+timeChoose.pack()
+
+# Register callback function
+reg = root.register(callback)
+timeChoose.config(validate="key", validatecommand=(reg, '%P'))
+
+# Button
+LaunchButton = Button(root, text="Launch", command=launch).pack(side=LEFT)  # launch the timer
+QuitButton = Button(root, text="Quit", command=root.destroy).pack(side=LEFT)    # quit the window
+
+root.mainloop() # display the window
+        
 # do a complete user interface for the timer (asking to sending)
-# display the time elpased
+# display the time elpased --> V3
