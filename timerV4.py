3536e675b116dd49948852215ba7af3f2e764e94
qlovy <qlovy@sysnet.ch>
2024-01-28 20:57:52 +0100

    It works !

--- timerV4.py
+++ timerV4.py
@@ -1,62 +1,63 @@
-'''
+"""
 TimerV4,
 goals: user interface with tkinter and countdown of the time choose by the user
-'''
+"""
 
 import time  # for manage the time
-from tkinter import * # for the user interface
+from tkinter import *  # for the user interface
+
 
 def read_entry():
-    global display
+    global display  # for using it in the function
     if entry.get().isdigit():
         # Launch the process
-        print("OK")
-        error_message.destroy()
+        error_message.destroy()  # remove the error message
         launch_timer()
     else:
-        # ask for an other input
-        print("not OK")
-        if display == False:
+        # ask for other input
+        if not display:
             error_message.pack()
             display = True
 
+
 def launch_timer():
-    timeChoose = int(entry.get())
-    start_seconds = time.time()
-    current_seconds = time.time()
-    perfect_seconds = 0
-    countdown = Tk()
-    countdown.geometry("200x200")
-    start_message = Label(countdown, text="Time start now")
-    start_message.pack()
-    while current_seconds - start_seconds < timeChoose:
-        time_elapse = round(current_seconds - start_seconds)
-        if time_elapse > perfect_seconds:
-            '''
+    time_choose = int(entry.get())  # convert the value of the entry
+    start_message = Label(window, text="Time starts now")
+    start_message.pack(pady=(40, 10))
+    start_seconds = time.time()  # the reference time
+    current_seconds = time.time()  # the current time
+    time_elapse = round(current_seconds - start_seconds)
+    perfect_seconds = 0  # use for the perfect "change" of second
+    while time_elapse < time_choose:
+        time_elapse = round(current_seconds - start_seconds)  # update the value
+        message = "Time elapsed: " + str(time_elapse)
+        if time_elapse != perfect_seconds:  # detection of one second being elapsed
+            # if we are at the start of the countdown
             if perfect_seconds != 0:
-                time_elapse_label.destroy()
-            '''
-            time_elapse_label = Label(countdown, text=time_elapse)
-            time_elapse_label.pack()
-            perfect_seconds = time_elapse
-        current_seconds = time.time()
-    stop_message = Label(countdown, text="Time finish")
-    stop_message.pack()
-    countdown.mainloop()
+                time_spend.destroy()  # remove the message
+            time_spend = Label(window, text=message)
+            time_spend.pack()  # display the message of the time elapse
+            perfect_seconds = time_elapse  # update the value
+        current_seconds = time.time()  # get the current time
+        window.update()
+    end_message = Label(window, text="Time is finished")
+    end_message.pack(pady=10)
+
 
 display = False
+
 # user interface
 window = Tk()
 
 # dimension of the window
-window.geometry("500x600")
+window.geometry("500x400")
 
 # title of the window
-title = Label(window, text = "Welcome in Timer V4", font=("Arial", 25))
+title = Label(window, text="Welcome in Timer V4", font=("Arial", 25))
 title.pack(padx=50, pady=20)
 
 # entry text and entry
-entry_text = Label(window, text = "How many seconds you want to elapse ?")
+entry_text = Label(window, text="How many seconds you want to elapse ?")
 entry_text.pack()
 entry = Entry(window)
 entry.pack(pady=10)
@@ -64,35 +65,10 @@ entry.pack(pady=10)
 # error message
 error_message = Label(window, text="Make sure to write an number")
 
-#button
-submit_button = Button(window, text="Submit", command=read_entry).pack(pady=10)
-quit_button = Button(window, text="Quit", command=window.destroy).pack()
+# button
+submit_button = Button(window, text="Submit", command=read_entry)
+submit_button.pack(pady=10)
+quit_button = Button(window, text="Quit", command=window.destroy)
+quit_button.pack()
 
 window.mainloop()
-'''
-# asking user
-while True:
-    try:
-        # user input
-        timeToElapse = 
-    except TypeError:   # if it isn't a number
-        print("Make sure to write number")
-    else:
-        # success message
-        print("The time has been correcty store")
-        break
-
-print("\nTime begin to elapse...")
-startSeconds = time.time()  # get the number of seconds since the epochs
-currentSeconds = time.time()
-oldSecondsElapsed = 0
-# until the time select by the user is elapsed
-while currentSeconds - startSeconds < timeToElapse:
-    secondsElapsed = round(currentSeconds - startSeconds)   # the current time elapsed, round to an integer
-    # when the perfectly one second has been elapsed
-    if secondsElapsed > oldSecondsElapsed:
-        print(secondsElapsed, "seconds has been elapsed", end="\r")
-        oldSecondsElapsed = round(currentSeconds - startSeconds)    # update the variable for the next time
-    currentSeconds = time.time()    # always update the current time
-print("\nTime finish to elapse")
-'''
